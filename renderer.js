49e875560f48b6f728937f013ae9651cc30babb4
Lucas Inácio Viegas <lucas.viegas@edu.pucrs.br>
2024-10-18 15:12:16 -0300

    Removida função novoGrafico

--- renderer.js
+++ renderer.js
@@ -48,34 +48,3 @@ window.onload = function () {
     e.preventDefault();
   })
 };
-
-function novoGrafico(telaId, titulo, estilo) {
-  const tipo = estilo || 'line';
-  const ctx = document.getElementById(telaId);
-  new Chart(ctx, {
-    type: tipo,
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      },
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        title: {
-          display: true,
-          text: titulo,
-        }
-      }
-    }
-  });
-}
